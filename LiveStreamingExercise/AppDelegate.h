393000d3a52d4ea26c4e561b73ffe8680b66c5e9
Rex <rex.huang@ehsn.com.tw>
2017-02-15 12:23:40 +0800

    建立專案

---
+++ LiveStreamingExercise/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LiveStreamingExercise
+//
+//  Created by Rex on 2017/2/15.
+//  Copyright © 2017年 Rex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LiveStreamingExercise/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LiveStreamingExercise
+//
+//  Created by Rex on 2017/2/15.
+//  Copyright © 2017年 Rex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
